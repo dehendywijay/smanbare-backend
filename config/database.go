e89e4572045a6c7d869bdbfc423593e499a93908
dehendyw <dehendywijaya2@gmail.com>
2026-05-11 12:52:07 +0700

    fix: database

--- config/database.go
+++ config/database.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -13,27 +12,24 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Println(".env tidak ditemukan")
+	if os.Getenv("VERCEL") == "" {
+		_ = godotenv.Load()
 	}
 
 	dsn := os.Getenv("DATABASE_URL")
-
-	if os.Getenv("IS_PRODUCTION") == "true" {
-		dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
+	if dsn == "" {
+		fmt.Println("DATABASE_URL belum diset")
+		os.Exit(1)
 	}
 
-	db, err := gorm.Open(postgres.New(postgres.Config{
-		DSN: dsn,
-		PreferSimpleProtocol: true,
-	}), &gorm.Config{
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		PrepareStmt: false,
 	})
 	if err != nil {
-		log.Fatal("Gagal konek database:", err)
+		fmt.Println("Gagal konek database:", err)
+		os.Exit(1)
 	}
 
 	DB = db
-	log.Println("Database connected ✅")
+	fmt.Println("Database connected ✅")
 }
