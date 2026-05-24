c990784683c95f6c4bb634fbb945712a93d2547f
dehendyw <dehendywijaya2@gmail.com>
2026-05-24 22:43:50 +0700

    fix

--- middleware/cors.go
+++ middleware/cors.go
@@ -24,7 +24,7 @@ func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		fmt.Println("CORS middleware hit:", c.Request.Method, c.Request.URL.Path)
 
-		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Header("Access-Control-Allow-Origin", "https://web-sma-xi.vercel.app")
 		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization")
 		c.Header("Access-Control-Allow-Credentials", "true")
