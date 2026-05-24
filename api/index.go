f66553d38d1ec64b5a39f9896c099ddc451637e9
dehendyw <dehendywijaya2@gmail.com>
2026-05-25 00:36:56 +0700

    fix

--- api/index.go
+++ api/index.go
@@ -1,12 +1,56 @@
 package handler
 
 import (
-	"gin-app/cmd/server"
+	"gin-app/config"
+	"gin-app/middleware"
+	"gin-app/models"
+	"gin-app/routes"
+	"log"
 	"net/http"
+	"sync"
+
+	"github.com/gin-gonic/gin"
 )
 
-var r = server.SetupRouter()
+var (
+	router *gin.Engine
+	once   sync.Once
+)
 
-func Handler(w http.ResponseWriter, req *http.Request) {
-	r.ServeHTTP(w, req)
+func setupRouter() {
+	gin.SetMode(gin.ReleaseMode)
+
+	config.ConnectDB()
+
+	err := config.DB.AutoMigrate(
+		&models.News{},
+		&models.Admin{},
+		&models.Guru{},
+		&models.KepalaSekolah{},
+		&models.Eskul{},
+		&models.Alumni{},
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := gin.New()
+
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+	r.Use(middleware.CORSMiddleware())
+
+	routes.NewsRoute(r)
+	routes.AuthRoute(r)
+	routes.GuruRoute(r)
+	routes.EskulRoute(r)
+	routes.AlumniRoute(r)
+
+	router = r
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	once.Do(setupRouter)
+	router.ServeHTTP(w, r)
 }
