365246ba40af89424392ab3da0f9c19c550bfb1b
dehendyw <dehendywijaya2@gmail.com>
2026-05-24 23:44:55 +0700

    fix

--- api/index.go
+++ api/index.go
@@ -1,4 +1,4 @@
-package main
+package handler
 
 import (
 	"gin-app/cmd/server"
--- dev-main.go
+++ dev-main.go
@@ -1,53 +1,12 @@
 package main
 
 import (
-	"gin-app/config"
-	"gin-app/middleware"
-	"gin-app/models"
-	"gin-app/routes"
-	"log"
-	"os"
-
-	"github.com/gin-gonic/gin"
+	"gin-app/cmd/server"
+	"net/http"
 )
 
-func main() {
-	gin.SetMode(gin.ReleaseMode)
-	
-	config.ConnectDB()
+var r = server.SetupRouter()
 
-	err := config.DB.AutoMigrate(
-		&models.News{},
-		&models.Admin{},
-		&models.Guru{},
-		&models.KepalaSekolah{},
-		&models.Eskul{},
-		&models.Alumni{},
-	)
-	if err != nil {
-		log.Fatal("Gagal migrasi database:", err)
-	}
-
-	r := gin.New()
-
-	r.Use(gin.Logger())
-	r.Use(gin.Recovery())
-	r.Use(middleware.CORSMiddleware())
-
-	routes.NewsRoute(r)
-	routes.AuthRoute(r)
-	routes.GuruRoute(r)
-	routes.EskulRoute(r)
-	routes.AlumniRoute(r)
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8081"
-	}
-
-	log.Println("Server running on port", port)
-
-	if err := r.Run(":" + port); err != nil {
-		log.Fatal("Gagal menjalankan server:", err)
-	}
+func Handler(w http.ResponseWriter, req *http.Request) {
+	r.ServeHTTP(w, req)
 }
