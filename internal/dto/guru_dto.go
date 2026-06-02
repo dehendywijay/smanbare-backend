07e3d5e0e0f016736bd0fc563a28e870df5b4ed2
Dehendy Wijaya <143471727+dehendywijay@users.noreply.github.com>
2026-06-02 16:28:12 +0700

    fix: dto request edit

--- internal/dto/guru_dto.go
+++ internal/dto/guru_dto.go
@@ -5,8 +5,9 @@ import "mime/multipart"
 type KepalaSekolahRequest struct {
 	Name    string `form:"name" binding:"required"`
 	Content string `form:"content" binding:"required"`
-	Foto   *multipart.FileHeader `form:"foto" binding:"required"`
+	Foto   *multipart.FileHeader `form:"foto" `
 }
+
 
 type GuruRequest struct {
 	Nama     string `form:"nama" binding:"required"`
